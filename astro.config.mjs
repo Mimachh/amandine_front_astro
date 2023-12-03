b74b987b2687772cc23f7188242791b217828526
unknown <karl.mullr@gmail.com>
2023-12-03 22:23:55 +0100

    preparation du sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.amandine-nails.fr/',
   integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  })],
+    applyBaseStyles: false
+  }), sitemap()]
   // output: "hybrid"
 });
